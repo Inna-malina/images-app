0e80323d710466961ecd3c3bc834cb92ca68786d
Inna Cherkashina <inna.cherkashina.1979@mail.ru>
2022-02-12 19:33:40 +0700

    feat: add some chainges

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let input = document.querySelector('input');
 let button = document.querySelector('.button__search');
 let clear = document.querySelector('.button__clear');
 let mainSection = document.querySelector('.main__section');
-// let images = document.querySelectorAll('.gallery-img');
+
 
 
 
 async function getData() {
-    let url = `https://api.unsplash.com/search/photos?query=${input.value}&client_id=P0dh6fg8wPyIw3B8dV5qHsrSAR-Mq_s7Xq7nCKx8r9s`;
+    let url = `https://api.unsplash.com/search/photos?query=${input.value}&client_id=P0dh6fg8wPyIw3B8dV5qHsrSAR-Mq_s7Xq7nCKx8r9s&per_page=30`;
 
     const res = await fetch(url);
     const data = await res.json();
@@ -30,18 +30,11 @@ async function getData() {
         }
     }
 
-    let images = document.querySelectorAll('.gallery-img');
-    images.forEach(function (elem) {
-
-        elem.addEventListener('click', function () {
-            elem.classList.toggle('gallery-img-big');
-            console.log('qwer');
-        });
-    });
+    creatModalWindow();
 }
 
 async function getDataRandom() {
-    let url = `https://api.unsplash.com/search/photos?query=car&client_id=P0dh6fg8wPyIw3B8dV5qHsrSAR-Mq_s7Xq7nCKx8r9s`;
+    let url = `https://api.unsplash.com/search/photos?query=car&client_id=P0dh6fg8wPyIw3B8dV5qHsrSAR-Mq_s7Xq7nCKx8r9s&per_page=30`;
 
     const res = await fetch(url);
     const data = await res.json();
@@ -52,20 +45,18 @@ async function getDataRandom() {
         for (let key in arr) {
             let elem = arr[key].urls.regular;
 
-            const img = `<img class="gallery-img" src="${elem}" alt="image">`;
+            const img = `<img class="gallery-img" src="${elem}" alt="image" data-img="img">`;
             galleryContainer.insertAdjacentHTML('beforeend', img);
+
+
+
+
+
         }
     }
 
-    let images = document.querySelectorAll('.gallery-img');
-    images.forEach(function (elem) {
+    creatModalWindow();
 
-        elem.addEventListener('click', function () {
-
-            elem.classList.toggle('gallery-img-big');
-            console.log('qwer');
-        });
-    });
 }
 getDataRandom();
 
@@ -109,6 +100,26 @@ function removeElements() {
     });
 }
 
+//функции для формирования модального окна при клике на картинку
+function creatModalWindow() {
+    let images = document.querySelectorAll('.gallery-img');
+    let modalWindowSection = document.querySelector('.modalwindow__section');
+    let modalWindowBox = document.querySelector('.modalwindow__section-box');
+
+    images.forEach(function (elem) {
+        elem.addEventListener('click', function () {
+
+            modalWindowBox.style.backgroundImage = `url('${elem.src}')`;
+            modalWindowSection.classList.remove('modal__hidden');
+        });
+    });
+
+    modalWindowSection.addEventListener('click', function () {
+        modalWindowSection.classList.add('modal__hidden');
+        modalWindowSection.classList.remove('modal__active');
+    });
+}
+
 console.log("на странице есть несколько фото и строка поиска +5");
 console.log("в футере приложения есть ссылка на гитхаб автора приложения, год создания приложения, логотип курса со ссылкой на курс +5");
 console.log("При загрузке приложения на странице отображаются полученные от API изображения +10");
@@ -118,4 +129,4 @@ console.log("есть placeholder +5");
 console.log("автозаполнение поля ввода отключено (нет выпадающего списка с предыдущими запросами) +5");
 console.log("после отправки поискового запроса и отображения результатов поиска, поисковый запрос продолжает отображаться в поле ввода +5");
 console.log("в поле ввода есть крестик при клике по которому поисковый запрос из поля ввода удаляется и отображается placeholder +5");
-console.log("Качество приложения не супер пупер, но вполне адекватное. Кое-какие мелкие дополнения от себя внесла");
+console.log("Качество приложения не супер пупер, но вполне адекватное. Кое-какие мелкие дополнения от себя внесла, например при клике на картинку возникает модальное окно с её увеличенным изображением");
